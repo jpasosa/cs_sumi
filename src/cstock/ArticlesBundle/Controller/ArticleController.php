5b9b9beef52ef7834b8ce2d2bb8bd3177f050fe0
jpasosa <jpasosa@gmail.com>
2013-06-19 18:01:17 -0300

    ArticlesBundle, create and starting list

--- src/cstock/ArticlesBundle/Controller/ArticleController.php
+++ src/cstock/ArticlesBundle/Controller/ArticleController.php
@@ -7,6 +7,7 @@ namespace cstock\ArticlesBundle\Controller;
 use cstock\ArticlesBundle\Entity\Articles;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,16 +16,21 @@ use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceListInterface;
 class ArticleController extends Controller {
     
     /**
-     * @Route("/article_new", name="article_new")
-     * @Template()
-     */
-    public function newAction(Request $request) {
+    * @Route("/article/new", name="article_new")
+    * @Method({"GET", "POST"})
+    * @Template()
+    */
+    public function newAction(Request $request)
+    {
         
         $new_article = new Articles();
-        $new_article->setCode('987736');
+        // $new_article->setCode('987736');
+
+
 
         $form = $this->createFormBuilder($new_article)
-                                    ->add('name', 'text', array('required' => false, 'label' => 'Nombre: '))
+                                    ->add('name', 'text', array('required' => false, 'label' => 'Nombre: ',
+                                                                            'attr'=>array('oninvalid'=>"setCustomValidity('Would you please enter a valid email?')")))
                                     ->add('code', 'text', array('label' => 'Código: '))
                                     ->add('description', 'textarea', array('required' => false, 'label' => 'Descripción: '))
                                     ->add('reference', 'text', array('required' => false, 'label' => 'Referencia: '))
@@ -34,22 +40,39 @@ class ArticleController extends Controller {
                                                                                                   3 => 'Muebles',
                                                                                                   4 => 'Papeles')))
                                     ->getForm();
-        if ($request->isMethod('POST')) {
-            $form->bind($request);
+        $form->handleRequest($request);
+
+            
             if($form->isValid()) {
+                
                 $em = $this->getDoctrine()->getManager();
-                // $new_article = $form->getData();
+                //$new_article = $form->getData();
                 $em->persist($new_article);
                 $em->flush();
 
                 return $this->render('cstockArticlesBundle:Article:new_created_ok.html.twig');
             }
 
-        }
 
         return $this->render('cstockArticlesBundle:Article:new.html.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+
+    /**
+    * @Route("/article/list", name="article_list")
+    * @Method({"GET", "POST"})
+    * @Template()
+    */
+    public function listAction()
+    {
+        
+        $new_article = new Articles();
+        $repository = $this->getDoctrine()->getRepository('cstockArticlesBundle:Articles');
+        $articles = $repository->findAll();
+        return $this->render('cstockArticlesBundle:Article:list.html.twig',
+                                        array('articles' => $articles));
     }
 
 
